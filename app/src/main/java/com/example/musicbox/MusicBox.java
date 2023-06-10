1daee81291b1f78d06b86a1d69b30b269e4cf52a
IljaRukin <ilja.rukin@gmail.com>
2023-06-10 16:10:38 +0200

    renaming project

--- app/src/main/java/com/example/musicbox/MusicBox.java
+++ app/src/main/java/com/example/musicbox/MusicBox.java
@@ -1,11 +1,10 @@
-package com.example.easytutomusicapp;
+package com.example.musicbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
